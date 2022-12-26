704f02204d66672a4bdd58d7b0be6a0daf057c8a
Manuel Alanis <manuelaag99@gmail.com>
2022-12-26 17:42:41 -0600

    Self and clicked functions

--- game.js
+++ game.js
@@ -8,21 +8,34 @@ $(document).keypress(function() {
     var press = event.key;
     if (press === "a") {
         var randomNumber = Math.floor((Math.random()*4));
-    var randomChosenColor = buttonColors[randomNumber];
-    console.log(randomChosenColor);
-    $("#" + [randomChosenColor]).addClass("pressed");
-    setTimeout(function() {
-        $("#" + [randomChosenColor]).removeClass("pressed");
-    }, 200);
-    var sound = new Audio("sounds/" + randomChosenColor + ".mp3");
-    sound.play();
+        var randomChosenColor = buttonColors[randomNumber];
+        console.log(randomChosenColor);
+        var sound = new Audio("sounds/" + randomChosenColor + ".mp3");
+        sound.play();
+        $("#" + [randomChosenColor]).addClass("pressed");
+        setTimeout(function() {
+            $("#" + [randomChosenColor]).removeClass("pressed");
+        }, 200);
     }
     else {
         console.log(press);
     }
+
 })
 
+$(".row .btn").click(function() {
+    //the "buttonID" variable stores the particular identity of the clicked button
+    var buttonID = this.id;
+    console.log(buttonID);
+    $("#" + [buttonID]).addClass("pressed");
+    var sound = new Audio("sounds/" + buttonID + ".mp3");
+    sound.play();
+    setTimeout(function() {
+        $("#" + [buttonID]).removeClass("pressed");
+    }, 200);
+    
 
+})
 
 //selectedButton.addClass("pressed");
 
