8044df49c6a58e235af66aa9ce52cddde1028f21
Manuel Alanis <manuelaag99@gmail.com>
2022-12-25 19:26:06 -0600

    Added JS

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-alert("alert");
+//this first array refers to classes on the HTML file
+buttonColors = ["red", "yellow", "blue", "green"];
+//this second array stores the order of colors for the game to operate
+gamePattern = [];
+
+var randomNumber = Math.floor((Math.random()*4));
+    randomChosenColor = buttonColors[randomNumber];
+    selectedButton = $("." + [randomChosenColor]).css("color", "black");
+    console.log(randomChosenColor);
+
+function nextSequence() {
+    var randomNumber = Math.floor((Math.random()*4));
+    randomChosenColor = buttonColors[randomNumber];
+    console.log(randomChosenColor);
+}
